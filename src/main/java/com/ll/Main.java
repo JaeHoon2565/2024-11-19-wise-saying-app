904dfa19075ebc2002621ef0b9a3309f6d939fdd
Hoon <xianse10@gmail.net>
2024-11-19 14:16:37 +0900

    App class

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,19 @@
+package com.ll;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+    }
+}
+class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
+    }
+
+}
